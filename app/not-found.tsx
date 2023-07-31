7a0568e7481b799b42a439d42e514f5426cec9d6
Julian Koehn <me@julian.pro>
2023-07-31 10:52:21 +0200

    feat: animations

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,20 @@
-import { Boundary } from '#/ui/boundary'
+import { Container } from "#/ui/Container";
 
 export default function NotFound() {
   return (
-    <Boundary labels={['not-found.tsx']} color="pink">
-      <div className="space-y-4 text-vercel-pink">
-        <h2 className="text-lg font-bold">Not Found</h2>
-
-        <p className="text-sm">Could not find requested resource</p>
-      </div>
-    </Boundary>
-  )
+    <section>
+      <Container className="flex min-h-screen items-center">
+        <div>
+          <p className="text-default text-sm font-medium">404 error</p>
+          <h1 className="text-default mt-3 text-2xl font-semibold md:text-3xl">
+            We can&apos;t find that page
+          </h1>
+          <p className="text-default mt-4">
+            Sorry, the page you are looking for doesn't exist or has been moved.
+          </p>
+          <div className="mt-6 flex items-center gap-x-3"></div>
+        </div>
+      </Container>
+    </section>
+  );
 }
